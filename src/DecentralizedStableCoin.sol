89abe2e07f3cd6fa2ea550965db70cd259ec93c4
tklasnakov <t.klasnakov@gmail.com>
2025-02-11 08:01:02 +0200

    Create the ERC20 token

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC20, ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title DecentralizedStableCoin 
+ * @author Todor Klasnakov
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+*/
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+	error DecentralizedStableCoin__MystBeMoreThanZero();
+	error DecentralizedStableCoin__BurnAmountExceedBalance();
+	error DecentralizedStableCoin__NotZeroAddress();
+
+	constructor()
+	ERC20("DecentralizedStableCoin", "DSC")
+	Ownable(msg.sender) {}
+
+	function burn(uint256 _amount) public override onlyOwner {
+		uint256 balance = balanceOf(msg.sender);
+		if (_amount <= 0) {
+			revert DecentralizedStableCoin__MystBeMoreThanZero();
+		}
+
+		if (balance < _amount) {
+			revert DecentralizedStableCoin__BurnAmountExceedBalance();
+		}
+
+		super.burn(_amount);
+	}
+
+	function mint(address _to, uint256 _amount) external onlyOwner returns(bool) {
+		if(_to == address(0)) {
+			revert DecentralizedStableCoin__NotZeroAddress();
+		}
+
+		if(_amount <= 0) {
+			revert DecentralizedStableCoin__MystBeMoreThanZero();
+		}
+
+		_mint(_to, _amount);
+
+		return true;
+	}
+}
