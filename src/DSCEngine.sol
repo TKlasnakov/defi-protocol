9c8070b2c713f06d2f854641b10a3a452d01a033
tklasnakov <t.klasnakov@gmail.com>
2025-02-15 19:59:07 +0200

    Add mintFunction and heltFunction to check for possible liquidation

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -23,6 +23,14 @@ contract DSCEngine is ReentrancyGuard {
 	error DSCEngine__TokenNotAllowed();
 	error DSCEngine__TokenAddressesAndPriceFeedsAddressesMustBeTheSameLength();
 	error DSCEngine__TransferFaild();
+	error DSCEngine__BreaksHeltFactor(uint256 userHeltFactor);
+	error DSCEngine__MintFailed();
+
+	uint256 private constant ADDITIONAL_FEE_PRECISION = 1e10;
+	uint256 private constant PRECISION = 1e16;
+	uint256 private constant LIQUDITION_THRESHOLD = 50;
+	uint256 private constant LIQUDITION_PRECISION = 100;
+	uint8 private constant MIN_HELT_FACTOR = 1;
 
 	mapping(address token => address priceFeed) private s_priceFeeds;
 	mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
@@ -102,6 +110,12 @@ contract DSCEngine is ReentrancyGuard {
 
 	function mintDsc(uint256 _amountDscToMint) external moreThanZero(_amountDscToMint) nonReentrant {
 		s_amountDscMinted[msg.sender] += _amountDscToMint;
+		_revertIfHeltFactorIsBroken(msg.sender);
+		bool minted = i_dsc.mint(msg.sender, _amountDscToMint);
+
+		if(!minted) {
+			revert DSCEngine__MintFailed();
+		}
 	}
 
 	function burnDsc() external {
@@ -116,32 +130,45 @@ contract DSCEngine is ReentrancyGuard {
 		// Get health factor
 	}
 
-	function _getAccountInformationOfTheUser(address user) private view returns(uint256 totalDscMinted, uint256 collateralValueInUsd) {
-		totalDscMinted = s_amountDscMinted[user];
-		
+	function _getAccountInformationOfTheUser(address _user) private view returns(uint256 totalDscMinted, uint256 collateralValueInUsd) {
+		totalDscMinted = s_amountDscMinted[_user];
+		collateralValueInUsd = getAccountCollateralValue(_user);
 	}
 
 	/*
 	 * Return how close to liquidation a user is
 	 * If a user goes bellow 1, then they can get liquidated
 	 */
-	function _heltFactor() private view returns (uint256) {
+	function _heltFactor(address _user) private view returns (uint256) {
+		(uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformationOfTheUser(_user);
+		uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUDITION_THRESHOLD) / LIQUDITION_PRECISION;
 		
+		return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
 	}
 
-	function _revertIfHeltFactorIsBroken() internal view {
-		
+	function _revertIfHeltFactorIsBroken(address _user) internal view {
+		uint256 userHeltFactor = _heltFactor(_user);
+
+		if(userHeltFactor < MIN_HELT_FACTOR) {
+			revert DSCEngine__BreaksHeltFactor(userHeltFactor);
+		}
 
 	}
 
-	function getAccountCollateralValue(address user) public view returns(uint256) {
+	function getAccountCollateralValue(address _user) public view returns(uint256 totalCollateralValueInUsd) {
 		for(uint256 i = 0; i < s_collateralTokens.length; i++) {
 			address token = s_collateralTokens[i];
-			uint256 amount = s_collateralDeposited[user][token];
+			uint256 amount = s_collateralDeposited[_user][token];
+			totalCollateralValueInUsd += getTokenUsdValue(token, amount);
 		}
+
+		return totalCollateralValueInUsd;
 	}
 
-	function getTokenUsdValue(address token, uint256 amount) public view returns(uint256) {
-		
+	function getTokenUsdValue(address _token, uint256 _amount) public view returns(uint256) {
+		AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[_token]);
+		(, int256 price,,,) = priceFeed.latestRoundData();
+
+		return ((uint256(price) * ADDITIONAL_FEE_PRECISION) * 10) / PRECISION;
 	}
 }
