31b8174b45ea423c1be15750a5d5cdf76420741a
tklasnakov <t.klasnakov@gmail.com>
2025-02-16 17:47:23 +0200

    forge fmt

--- script/DeployDecentralizedStableCoin.s.sol
+++ script/DeployDecentralizedStableCoin.s.sol
@@ -1,31 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { Script } from "forge-std/Script.sol";
-import { DSCEngine } from "../src/DSCEngine.sol";
-import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
-import { HelperConfig } from "./HelperConfig.s.sol";
+import {Script} from "forge-std/Script.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDecentralizedStableCoin is Script {
-	address[] public tokenAddresses;
-	address[] public priceFeedAddresses;
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
-	function run() external returns(DSCEngine, DecentralizedStableCoin, HelperConfig) {
-		HelperConfig helperConfig = new HelperConfig();
-		(address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = helperConfig.activeNetworkConfig();
+    function run() external returns (DSCEngine, DecentralizedStableCoin, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
 
-		tokenAddresses = [weth, wbtc];
-		priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
-		vm.startBroadcast(deployerKey);
+        vm.startBroadcast(deployerKey);
 
-		DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-		DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
 
-		dsc.transferOwnership(address(dscEngine));
+        dsc.transferOwnership(address(dscEngine));
 
-		vm.stopBroadcast();
+        vm.stopBroadcast();
 
-		return (dscEngine, dsc, helperConfig);
-	}
+        return (dscEngine, dsc, helperConfig);
+    }
 }
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,44 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { Test } from "forge-std/Test.sol";
-import { DeployDecentralizedStableCoin } from "../../script/DeployDecentralizedStableCoin.s.sol";
-import { DSCEngine } from "../../src/DSCEngine.sol";
-import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
-import { HelperConfig } from "../../script/HelperConfig.s.sol";
-import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeployDecentralizedStableCoin} from "../../script/DeployDecentralizedStableCoin.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
-	DSCEngine dscEngine;
-	DecentralizedStableCoin dsc;
-	HelperConfig config;
-	address ethUsdPriceFeed;
-	address weth;
+    DSCEngine dscEngine;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
 
-	address public USER = makeAddr("user");
-	uint256 public constant AMOUNT_COLLATERAL = 10 ether;
-	uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
-	function setUp() public {
-		DeployDecentralizedStableCoin deploy = new DeployDecentralizedStableCoin();
-		(dscEngine, dsc, config) = deploy.run();	
-		(ethUsdPriceFeed, , weth, ,) = config.activeNetworkConfig();
-		ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
-	}
+    function setUp() public {
+        DeployDecentralizedStableCoin deploy = new DeployDecentralizedStableCoin();
+        (dscEngine, dsc, config) = deploy.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
 
-	function testGetUsdValue() public view {
-		uint256 ethAmount = 15e18;
-		uint256 expectedAmount = 30000e18;
-		uint256 actualUsd = dscEngine.getTokenUsdValue(weth, ethAmount);
+    function testGetUsdValue() public view {
+        uint256 ethAmount = 15e18;
+        uint256 expectedAmount = 30000e18;
+        uint256 actualUsd = dscEngine.getTokenUsdValue(weth, ethAmount);
 
-		assertEq(expectedAmount, actualUsd);
-	}
+        assertEq(expectedAmount, actualUsd);
+    }
 
-
-	function testRvertsIfCollateralZero() public {
-		vm.expectRevert(DSCEngine.DSCEngine__MystBeMoreThanZero.selector);
-		dscEngine.depositCollateral(weth, 0);
-		vm.stopPrank();
-	}
+    function testRvertsIfCollateralZero() public {
+        vm.expectRevert(DSCEngine.DSCEngine__MystBeMoreThanZero.selector);
+        dscEngine.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
 }
-
